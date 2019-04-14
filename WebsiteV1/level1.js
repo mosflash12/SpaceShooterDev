9f89ede67d85b67b86aff3104fe0274f0934f64f
Dexter Manswell <mosflash12@gmail.com>
2019-04-14 12:41:30 -0400

    added a basic configuration...and a starting scene..however i ran into some problems making an image show up as a test...trying to find a solution

---
+++ WebsiteV1/level1.js
@@ -0,0 +1,15 @@
+class level1 extends Phaser.Scene {
+
+  constructor(){
+    super({key:"level1"});
+  }
+
+  preload(){
+    this.load.image('testimg', 'assets/testjpg.jpg');
+  }
+
+  create(){
+    this.image = this.add.image(400,300,'testimg');
+    }
+
+}
